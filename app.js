616890784251151d4df02b779db3d939c69af244
LuieR <rhluisenrique@gmail.com>
2025-04-04 20:05:48 -0400

    chore: implemented error handling w http-errors

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require('dotenv').config
+const mongoose = require("mongoose")
+const createError = require("http-errors")
 const express = require("express")
 const logger = require("morgan")
 
@@ -10,6 +12,34 @@ app.use(logger('dev'))
 
 const api = require("./config/routes.config")
 app.use("/v1", api)
+
+app.use((req, res, next) => {
+  next(createError(404, "Route not found"))
+})
+
+app.use((error, req, res, next) => {
+
+  if(error instanceof mongoose.Error.ValidationError){
+    error = createError(400, error)
+  } else {
+    error = createError(500, error)
+  }
+
+  error = error.status ? error : createError(500, error)
+  console.error(error);
+
+  const errors = !error.errors ? undefined : Object.keys(error.errors).reduce((errors, errorKey) => {
+    errors[errorKey] = error.errors[errorKey].message || error.errors[errorKey]
+    return errors
+  }, {})
+  
+  const data = {
+    message: error.message,
+    errors
+  }
+  res.status(error.status).json(data)
+
+})
 
 
 const port = process.env.PORT || 3000
